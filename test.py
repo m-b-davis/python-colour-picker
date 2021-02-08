e4458a931ec443a01fb4962515c74d847581374f
Matt Davis <matt.davis@truelayer.com>
2021-02-08 22:56:40 +0000

    Refactor code for readability & remove global mutation

--- test.py
+++ test.py
@@ -2,74 +2,107 @@ import numpy as np
 import pandas as pd
 import cv2
 
-clicked = False
-r = g = b = xpos = ypos = 0
-img = cv2.imread("colours.jpg")
-csv_columns=["color", "color_name", "hex", "R", "G", "B"]
-csv = pd.read_csv("colours.csv", names=csv_columns, header=None)
+# Config
+main_window_id = "Color Recognition App"
 
-font_colours = {
-	"white": (255, 255, 255),
-	"black": (0, 0, 0),
+# Files
+image_path = "colours.jpg"
+colours_csv_path = "colours.csv"
+
+# Text styles
+text_position = (50, 50)
+text_scale = 0.5
+text_thickness = 1
+text_family = cv2.FONT_HERSHEY_COMPLEX
+text_colours = {
+    "white": (255, 255, 255),
+    "black": (0, 0, 0),
 }
 
-def recognize_color(R,G,B):
-	minimum = 10000
-	for i in range(len(csv)):
-		d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"]))+ abs(B- int(csv.loc[i,"B"]))
-		if (d <= minimum):
-			minimum = d
-			cname = csv.loc[i, "color_name"]
-	return cname
+csv_columns = ["color", "color_name", "hex", "R", "G", "B"]
+csv = pd.read_csv(colours_csv_path, names=csv_columns, header=None)
 
-def mouse_click(event, x, y, flags, param):
-	if event == cv2.EVENT_LBUTTONDOWN:
-		print("Mouse clicked")
-		global b, g, r, xpos, ypos, clicked
-		clicked = True
-		xpos = x
-		ypos = y
-		b,g,r = img[y,x]
-		r = int(r) 
-		g = int(g)
-		b = int(b)
+def recognize_color(r, g, b):
+    def get_row_rgb_values(csv, i):
+      return (csv.loc[i, "R"], csv.loc[i, "G"], csv.loc[i, "B"])
 
-def add_text(img, text, colour):
-	text_position = (50, 50)
-	font_scale = 0.5
-	thickness = 1
-	cv2.putText(img, text, text_position, cv2.FONT_HERSHEY_COMPLEX, font_scale, colour, thickness, cv2.LINE_AA)
+    def get_diff(actual_colour, row_colour_string):
+        return abs(actual_colour - int(row_colour_string))
 
-def main():
-	global clicked
-	main_window_name = "Color Recognition App"
+    current_lowest_diff = 10000
+    colour_name = "Unknown!"
 
-	cv2.namedWindow(main_window_name)
-	cv2.setMouseCallback(main_window_name, mouse_click)
-	cv2.imshow(main_window_name, img)
+    for i in range(len(csv)):
+        row_r, row_g, row_b = get_row_rgb_values(csv, i)
 
-	while (1):
-		if (clicked):
-			clicked=False
-			#cv2.rectangle(img, startpoint, endpoint, color, thickness)-1 fills entire rectangle 
-			cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1) #Creating text string to display( Color name and RGB values )
-			text = recognize_color(r,g,b) + " R="+ str(r) +  " G="+ str(g) +  " B="+ str(b)
-		
-			#cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
-			print(text)
+        # Get diffs from image colour to row colour
+        r_diff = get_diff(r, row_r)
+        g_diff = get_diff(g, row_g)
+        b_diff = get_diff(b, row_b)
 
-			if (r + g + b >= 600):
-				# For very light colours we will display text in black colour
-				print("Add white text")
-				add_text(img, text, font_colours["white"])
-			else:
-				# Otherwise, black text
-				print("Add black text")
-				add_text(img, text, font_colours["black"])
-			
-		#Break the loop when user hits "esc" key    
-		if cv2.waitKey(20) & 0xFF ==27:
-			break
-	cv2.destroyAllWindows()
+        row_total_diff = r_diff + g_diff + b_diff
+    
+        # If this is the lowest row we've seen, use this as the current target row
+        if (row_total_diff <= current_lowest_diff):
+            current_lowest_diff = row_total_diff
+            colour_name = csv.loc[i, "color_name"]
+
+    return colour_name
+
+
+# This function creates a function which handles the event
+# This is so the handler has a reference to img
+def get_mouse_click_handler(img):
+    def handle_mouse_click(event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            b, g, r = img[y, x]
+            display_colour(img, int(r), int(g), int(b))
+
+    # Return the handler
+    return handle_mouse_click
+
+def display_colour(img, r, g, b):
+    def add_text(img, text, colour):
+      cv2.putText(img, text, text_position, text_family, text_scale, colour, text_thickness, cv2.LINE_AA)
+
+    # Background rectangle
+    cv2.rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
+
+    # Text to display - classification + rgb
+    text = f"Classification: {recognize_color(r,g,b)}, R={r} G={g} B={b}"
+
+    def is_bright_colour(r, g, b):
+      return r + g + b >= 400
+
+    text_colour = "white"
+
+    if is_bright_colour(r, g, b):
+      text_colour = "black"
+
+    add_text(img, text, text_colours[text_colour])
+
+# Draw loop and event handling
+def run_gui_loop(img):
+    while (1):
+        # Show the image - this acts like a render/draw function
+        cv2.imshow(main_window_id, img)
+
+        # Break the loop when user hits "esc" key
+        if cv2.waitKey(20) & 0xFF == 27:
+            break
+
+def main(): 
+    cv2.namedWindow(main_window_id)
+    img = cv2.imread(image_path)
+
+    # Create and setup event handler
+    handle_mouse_click = get_mouse_click_handler(img)
+    cv2.setMouseCallback(main_window_id, handle_mouse_click)
+
+    # Run gui loop
+    run_gui_loop(img)
+
+    # Quit once gui loop finishes
+    cv2.destroyAllWindows()
 
 main()
